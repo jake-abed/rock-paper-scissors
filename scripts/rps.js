0ac0bd4d231d00847ea8c6f573530ec179efe283
jake-abed <jakeabed@gmail.com>
2022-12-14 09:33:37 -0800

    Plan out basic game logic before coding

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,1 +1,7 @@
 "use strict";
+
+//The computer chooses it's option.
+//The user chooses their option.
+//The game compares the choices. Rock beats scissors, paper beats rock, and scissors beats paper.
+//The game returns the results.
+//Reset the game.
